c296a90d0d4f2318b98cbe3bbed07e1bb7b9586e
Daniel Flores <daniel.flores4930@gmail.com>
2022-07-26 13:55:20 -0700

    added some seed files and changes to model files to match migration files

--- backend/db/seeders/20220715162904-demo-user.js
+++ backend/db/seeders/20220715162904-demo-user.js
@@ -9,16 +9,37 @@ module.exports = {
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'Demo',
+        lastName: 'User',
+        host: true,
+        superHost: true,
+        identityVerified: true,
+        aboutMe: 'I am the ultimate user. I am the original. Numero Uno.',
+        duringStay: 'You better be clean to my place!'
       },
       {
         email: faker.internet.email(),
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        firstName: 'Fake',
+        lastName: 'User',
+        host: true,
+        superHost: false,
+        identityVerified: true,
+        aboutMe: 'I am a fake. I am just a regular host.',
+        duringStay: 'I apologize ahead of time if you hear me snore.'
       },
       {
         email: faker.internet.email(),
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        firstName: 'Fake',
+        lastName: 'User',
+        host: false,
+        superHost: false,
+        identityVerified: false,
+        aboutMe: 'Who am I?',
+        duringStay: 'Do whatever!'
       },
     ], {});
   },

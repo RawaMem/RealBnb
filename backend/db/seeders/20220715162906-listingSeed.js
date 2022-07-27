7113465c10fd434e242b82544c187de7b38186fa
Rawaha M <rawaha.m@gmail.com>
2022-07-26 17:13:43 -0700

    starting seed data

--- backend/db/seeders/20220715162906-listingSeed.js
+++ backend/db/seeders/20220715162906-listingSeed.js
@@ -7,11 +7,25 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      */
+   return queryInterface.bulkInsert('Listings', [
+    {
+      ownerId: '3',
+      previewImageUrl: false,
+      name: 'Hotel California',
+      description: 'Welcome to the Hotel California, such a lovely place, such a lovely face, plenty of room at the Hotel California, any time of year, you can find it here.',
+      serviceFee: 24.99,
+      cleaningFee: 29.99,
+      numRooms: 5,
+      maxGuests: 10,
+      address: '9876 Wilshire Blvd',
+      city: 'Beverly Hills',
+      state: 'CA',
+      zipCode: 90210,
+      longitude: '',
+      latitude: ''
+   },
+  ], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -20,7 +34,7 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      */
+   return queryInterface.bulkDelete('Listings', null, {});
   }
 };

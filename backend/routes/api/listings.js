23427166be8ca3d53f35fd6b7026c0f6045e908c
Rawaha M <rawaha.m@gmail.com>
2022-08-01 16:44:14 -0700

    refining listing query pair programming with Zhen and Rawaha

--- backend/routes/api/listings.js
+++ backend/routes/api/listings.js
@@ -8,33 +8,36 @@ const { requireAuth } = require('../../utils/auth');
 
 const { Op } = require('sequelize');
 
-const { Listing, ListingPrice, Review } = require('../../db/models');
+const { Listing, ListingPrice, Review, Category } = require('../../db/models');
 
 const router = express.Router();
 
 // get all listings
+//when wishlist is implemented, include wishlist so that we know which hearsts are filled in
 router.get('/', asyncHandler(async (req, res) => {
     const listings = await Listing.findAll({
-        attributes: [
-                  [Sequelize.fn('AVG', Sequelize.col('Reviews.starRating')), 'avgRating'],
+        // attributes: [
+        //           [Sequelize.fn('AVG', Sequelize.col('Reviews.starRating')), 'avgRating'],
 
-                ],
+        //         ],
         include: [
-                  {
-                    model: Review,
-                    // as: 'avgReview',
-                    attributes: [],
-                  },
-                  ListingPrice
+                  Review,
+                  // {
+                  //   model: Review,
+                  //   as: 'avgReview',
+                  //   attributes: [],
+                  // },
+                  ListingPrice,
+                  Category
                 ],
 
     });
-    console.log("=======this is listing=====")
+    console.log("=======this is listing=====", listings[0].Categories)
     return res.json(listings);
 }));
 
 
-
+//ask Alec how to do aggregate average rating for findall listings including reviews
 // Model.User.findOne({
 //     where: { id: 7 },
 //     attributes: [

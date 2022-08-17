b3b7a42b156770bd965894e9cccb42c1402ebfbc
sherryyu <bambooyu715@gmail.com>
2022-08-17 10:59:16 -0400

    dynamically rendering listing display based on category selection done

--- frontend/src/components/Listings/index.js
+++ frontend/src/components/Listings/index.js
@@ -5,12 +5,40 @@ import { csrfFetch } from '../../store/csrf';
 import { getListings } from '../../store/listings';
 import ListingCard from './ListingCard';
 
+
 export default function Listings() {
     const dispatch = useDispatch();
     const listingsObj = useSelector(state => state.listings);
-    let listings;
-    if (listingsObj) listings = Object.values(listingsObj);
+
     const [categories, setCategories] = useState(null);
+    const [sorted, setSorted] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+
+    let listings = handleListingsDisplay();
+
+    function handleListingsDisplay() {
+        if(listingsObj) {
+            let listingsToDispaly = Object.values(listingsObj);
+            if(!sorted) {
+                return listingsToDispaly
+            } else {
+                listingsToDispaly = listingsToDispaly.filter(listObj => {
+                    let categoryArr = listObj.Categories;
+                    console.log('categoryArr', categoryArr);
+                    for(let category of categoryArr) {
+                        console.log('category', category.name);
+                        console.log('boolean', category.name === selectedCategory);
+                        return category.name === selectedCategory;
+                    };
+                });
+                console.log('in the else', listingsToDispaly);
+                return listingsToDispaly;
+            };
+        }
+    };
+
+
+
     useEffect(() => {
         dispatch(getListings());
      }, [dispatch]);
@@ -27,7 +55,7 @@ export default function Listings() {
     function displayCategories() {
         return categories && categories.map(category => (
             <div key={category.id}>
-                <NavLink style={{textDecoration:'none', marginRight:"10px"}} to={'/listings'}>                  
+                <NavLink style={{textDecoration:'none', marginRight:"10px"}} to={'/listings'} onClick={() => {setSelectedCategory(category.name); setSorted(true)}}>                  
                     {category.name}                 
                 </NavLink>
             </div>

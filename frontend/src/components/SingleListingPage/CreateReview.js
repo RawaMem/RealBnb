937444dbfba094cc12c210d702ddd868a054a940
Rawaha M <rawaha.m@gmail.com>
2022-10-03 15:00:01 -0700

    fixed onChange in drop downs

--- frontend/src/components/SingleListingPage/CreateReview.js
+++ frontend/src/components/SingleListingPage/CreateReview.js
@@ -27,7 +27,7 @@ function CreateReview({setShowCreateReviewModal}) {
                     Overall Star Rating
                     <select
                     value={starRating}
-                    onChange={()=>setStarRating(e.target.value)}
+                    onChange={(e)=>setStarRating(e.target.value)}
                     className="starRatingDropDown">
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -40,7 +40,7 @@ function CreateReview({setShowCreateReviewModal}) {
                     Cleanliness Rating
                     <select
                     value={cleanliness}
-                    onChange={()=>setCleanliness(e.target.value)}
+                    onChange={(e)=>setCleanliness(e.target.value)}
                     className="cleanlinessDropDown">
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -53,7 +53,7 @@ function CreateReview({setShowCreateReviewModal}) {
                     Communication Rating
                     <select
                     value={communication}
-                    onChange={()=>setCommunication(e.target.value)}
+                    onChange={(e)=>setCommunication(e.target.value)}
                     className="communicationDropDown">
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -66,7 +66,7 @@ function CreateReview({setShowCreateReviewModal}) {
                     CheckIn Rating
                     <select
                     value={checkIn}
-                    onChange={()=>setCheckIn(e.target.value)}
+                    onChange={(e)=>setCheckIn(e.target.value)}
                     className="checkInDropDown">
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -79,7 +79,7 @@ function CreateReview({setShowCreateReviewModal}) {
                     Accuracy Rating
                     <select
                     value={accuracy}
-                    onChange={()=>setAccuracy(e.target.value)}
+                    onChange={(e)=>setAccuracy(e.target.value)}
                     className="accuracyDropDown">
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -92,7 +92,7 @@ function CreateReview({setShowCreateReviewModal}) {
                     Location Rating
                     <select
                     value={location}
-                    onChange={()=>setLocation(e.target.value)}
+                    onChange={(e)=>setLocation(e.target.value)}
                     className="locationDropDown">
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -105,7 +105,7 @@ function CreateReview({setShowCreateReviewModal}) {
                     Value Rating
                     <select
                     value={value}
-                    onChange={()=>setValue(e.target.value)}
+                    onChange={(e)=>setValue(e.target.value)}
                     className="valueDropDown">
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -119,7 +119,7 @@ function CreateReview({setShowCreateReviewModal}) {
                     cols="30"
                     rows="10"
                     value={content}
-                    onChange={()=> setContent(e.target.value)}
+                    onChange={(e)=> setContent(e.target.value)}
                     className="contentTextArea"></textarea>
                 </label>
                 <button className="createReviewSubmitBtn">Submit Review</button>

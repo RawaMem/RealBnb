397d56bc3412c5b147188ccc446ffa4f85c16ee1
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2023-06-10 15:19:49 -0500

    wishlist-frontend-map/ Adding onClick to the markers and opening a modal showing the listing.

--- frontend/src/components/Maps/Mapbox.js
+++ frontend/src/components/Maps/Mapbox.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import Map, { Marker } from "react-map-gl";
 import { useDispatch, useSelector } from "react-redux";
 import { getToken } from "../../store/maps";
+import { Modal } from "../../context/Modal";
 import "mapbox-gl/dist/mapbox-gl.css";
+import ListingCard from "../Listings/ListingCard";
 
 export function MapBox({
   style,
@@ -16,6 +18,7 @@ export function MapBox({
   const dispatch = useDispatch();
   const token = useSelector((state) => state.maps?.token);
   const [viewport, setViewport] = useState({ latitude, longitude, zoom: 12 });
+  const [showListing, setShowListing] = useState(false);
 
   useEffect(() => {
     if (!token) dispatch(getToken());
@@ -44,28 +47,39 @@ export function MapBox({
           >
             {validListings.has(item.id) ? (
               <div
+                onClick={() => setShowListing(item.id)}
                 style={{
                   fontSize: "15px",
                   color: "red",
                   fontWeight: "bold",
                   backgroundColor:
-                    hoveredListing !== null && hoveredListing === item.id ? "black" : "transparent",
+                    hoveredListing !== null && hoveredListing === item.id
+                      ? "black"
+                      : "transparent",
                 }}
               >
                 {item.ListingPrices[0].pricePerDay}
               </div>
             ) : (
               <span
+                onClick={() => setShowListing(item.id)}
                 style={{
                   fontVariationSettings:
                     "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 48",
                   backgroundColor:
-                    hoveredListing !== null && hoveredListing === item.id ? "black" : "transparent",
+                    hoveredListing !== null && hoveredListing === item.id
+                      ? "black"
+                      : "transparent",
                 }}
                 class="material-symbols-outlined"
               >
                 block
               </span>
+            )}
+            {showListing === item.id && (
+              <Modal onClose={() => setShowListing(null)}>
+                <ListingCard listing={item} />
+              </Modal>
             )}
           </Marker>
         ))
